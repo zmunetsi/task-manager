be961d3981e44a44c8182065d9be0fcb2b4e8629
Munetsi Zunguzira <dev@jetweb.co.za>
2020-02-25 18:32:02 +0200

    working sortable controller

--- app/Http/Controllers/SortableController.php
+++ app/Http/Controllers/SortableController.php
@@ -8,6 +8,11 @@ use App\Task;
 class SortableController extends Controller
 {
   
+      public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 
      public function sort(Request $request)
     {    
